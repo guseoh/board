97a7d6667be4be7ff4ba9b2e5239a72bf7a74514
guseoh <guseoh@naver.com>
2026-01-09 14:39:10 +0900

    feat: add @EnableJpaAuditing

--- src/main/java/project/board/BoardApplication.java
+++ src/main/java/project/board/BoardApplication.java
@@ -1,9 +1,12 @@
 package project.board;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardApplication {
 
 	public static void main(String[] args) {
