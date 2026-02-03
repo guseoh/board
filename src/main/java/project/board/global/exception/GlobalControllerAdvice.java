8802127c0332c9578f9a5db7beac214ac47681da
guseoh <guseoh@naver.com>
2026-02-03 18:32:02 +0900

    fix: addAttribute -> addFlashAttribute

--- src/main/java/project/board/global/exception/GlobalControllerAdvice.java
+++ src/main/java/project/board/global/exception/GlobalControllerAdvice.java
@@ -3,16 +3,19 @@ package project.board.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(CustomException.class)
-    public String customException(CustomException e) {
+    public String customException(CustomException e, RedirectAttributes ra) {
         ErrorCode errorCode = e.getErrorCode();
 
-        log.warn("Exception:  msg= {} ", errorCode.getMessage());
+        log.warn("Exception:  msg= {}", errorCode.getMessage());
+
+        ra.addFlashAttribute("msg", e.getMessage());
 
         return "redirect:" + errorCode.getRedirectUrl();
     }
