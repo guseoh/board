72eeb7cf364cb9f2183aabbbc5557a3c6610d38e
guseoh <guseoh@naver.com>
2026-02-09 17:34:27 +0900

    refactor: 예외처리 개선

--- src/main/java/project/board/global/exception/ErrorCode.java
+++ src/main/java/project/board/global/exception/ErrorCode.java
@@ -6,9 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+
+    // post
     LOGIN_REQUIRED("/loginForm", "로그인이 필요합니다"),
     POST_NOT_FOUND("/", "해당 게시글이 존재하지 않습니다."),
-    NOT_POST_OWNER("/", "본인 게시글만 수정/삭제 가능합니다");
+    NOT_POST_OWNER("/", "본인 게시글만 수정/삭제 가능합니다"),
+
+    // member
+    MEMBER_NOT_FOUND("/", "해당 사용자가 존재하지 않습니다."),
+    DUPLICATE_EMAIL("/signup", "이미 사용중인 이메일입니다."),
+    DUPLICATE_NICKNAME("/signup", "이미 사용중인 닉네임입니다.");
 
     private final String redirectUrl;
     private final String message;
