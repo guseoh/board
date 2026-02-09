a37a555353f3ec12cb6a44eb8c982990d4bbbc3a
guseoh <guseoh@naver.com>
2026-02-09 16:38:08 +0900

    feat: 게시글 검색 기능

--- src/main/java/project/board/post/repository/PostRepository.java
+++ src/main/java/project/board/post/repository/PostRepository.java
@@ -1,7 +1,15 @@
 package project.board.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import project.board.post.dto.PostDto;
 import project.board.post.entity.Post;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+    List<Post> findByTitleContaining(String keyword);
 }
