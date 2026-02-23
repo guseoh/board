3adc54ad5828dd4316355894649283605c0995a4
guseoh <guseoh@naver.com>
2026-02-23 15:40:13 +0900

    refactor: CustomException 적용

--- src/main/java/project/board/global/security/user/CustomUserDetailsService.java
+++ src/main/java/project/board/global/security/user/CustomUserDetailsService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import project.board.global.exception.CustomException;
+import project.board.global.exception.ErrorCode;
 import project.board.member.entity.Member;
 import project.board.member.repository.MemberRepository;
 
@@ -16,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) {
         Member member = memberRepository.findByEmail(username).orElseThrow(() ->
-                new IllegalArgumentException("User not found: " + username));
+                new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
         return new CustomUserDetails(
                 member.getNickname(),
