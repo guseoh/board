edb8a2c7cda7aa460e2481ac51e9743ba0e25ea6
guseoh <guseoh@naver.com>
2026-03-24 19:30:59 +0900

    refactor: PostDto Inner Class에서 목적별로 분리

---
+++ src/main/java/project/board/post/dto/request/PostRequest.java
@@ -0,0 +1,22 @@
+package project.board.post.dto.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class PostRequest {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
+
+}
