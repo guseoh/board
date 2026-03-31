e66158f2eb22c0626de475a850a6f5190e9e44ce
guseoh <81723538+guseoh@users.noreply.github.com>
2026-03-31 17:36:54 +0900

    feat: Admin 기능 (#65)
    * refctor: details 수정
    
    * feat: Admin 기능
    
    - 관리자 페이지 생성
    - 관리자 전체 게시글, 유저 수정/삭제 가능

--- src/main/java/project/board/comment/repository/CommentRepository.java
+++ src/main/java/project/board/comment/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import project.board.comment.entity.Comment;
+import project.board.member.entity.Member;
 
 import java.util.List;
 
@@ -15,4 +16,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Modifying
     @Query("delete from Comment c where c.post.id = :id")
     void deleteById(@Param("id") Long postId);
+
+    @Modifying
+    @Query("delete from Comment c where c.member = :member")
+    void deleteAllByMember(@Param("member") Member member);
+
+    @Modifying
+    @Query("delete from Comment c where c.post.member = :member")
+    void deleteAllByPostMember(@Param("member") Member member);
 }
