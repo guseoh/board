551dd63bd54ad2b2f8d779a303f6412bf8a84066
guseoh <guseoh@naver.com>
2026-04-24 16:35:11 +0900

    feat: Build.Default #72

--- src/main/java/project/board/global/dto/PageRequestDto.java
+++ src/main/java/project/board/global/dto/PageRequestDto.java
@@ -13,11 +13,20 @@ import org.springframework.data.domain.Sort;
 @Setter     // PageRequestDto 바인딩이 안 돼서 size=0으로 남는 경우
 public class PageRequestDto {
 
+    @Builder.Default
     private int size = 5;
+
+    @Builder.Default
     private int page = 1;
+
+    private String keyword;
 
     public Pageable getPageable(Sort sort) {
         return PageRequest.of(page - 1, size, sort);
     }
 
+
+    public boolean hasKeyword() {
+        return keyword != null && !keyword.isBlank();
+    }
 }
