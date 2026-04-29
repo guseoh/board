7691ffd2ed160fc11c22a1ce7b708b73e7ab55f6
guseoh <guseoh@naver.com>
2026-04-29 17:01:17 +0900

    :poop:

--- src/main/java/project/board/comment/service/CommentService.java
+++ src/main/java/project/board/comment/service/CommentService.java
@@ -85,14 +85,14 @@ public class CommentService {
                 .toList();
     }
 
-    @Transactional(readOnly = true)
-    public List<MyCommentPageResponse> myCommentPage(PageRequestDto requestDto) {
-
-        Long memberId = getLoginMemberId();
-
-        List<Comment> comments = commentRepository.MyComments(memberId);
-
-    }
+//    @Transactional(readOnly = true)
+//    public List<MyCommentPageResponse> myCommentPage(PageRequestDto requestDto) {
+//
+//        Long memberId = getLoginMemberId();
+//
+//        List<Comment> comments = commentRepository.MyComments(memberId);
+//
+//    }
 
 
     public Long myCommentCount(Long id) {
